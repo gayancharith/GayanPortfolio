8996fa8566baf368b953936053e42324d29f3cf2
Achini Gayanthika <agayanthika@gmail.com>
2020-08-27 17:41:06 +0530

    changes

--- src/app/widgets/sidenav/sidenav.component.ts
+++ src/app/widgets/sidenav/sidenav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, NavigationEnd, Router} from '@angular/router';
 
 @Component({
   selector: 'site-sidenav',
@@ -6,11 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidenav.component.scss']
 })
 export class SidenavComponent implements OnInit {
-  showToggleMenu: boolean = false;
-  toggleMenuOpacity: number = 0;
+  // showToggleMenu: boolean = false;
+  // toggleMenuOpacity: number = 0;
   isOpen: boolean;
+  activatedRouteIndex: number;
 
-  constructor() { }
+  constructor(private router: Router,
+              private route: ActivatedRoute) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.activatedRouteIndex = route.firstChild.routeConfig.data.index;
+      }
+    });
+  }
 
   ngOnInit(): void {
   }
