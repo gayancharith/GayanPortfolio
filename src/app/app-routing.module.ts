b663639ccfdc30a1fbaa69639f3c82bedea60d08
Gayan Witharana <vgayancharith@gmail.com>
2021-11-26 18:58:48 +0530

    remove contacts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
-import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
@@ -10,28 +10,27 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./pages/home/home.module').then((m) => m.HomeModule),
-    data: {index: 0}
+    data: { index: 0 },
   },
   {
     path: 'skills',
     loadChildren: () => import('./pages/skills/skills.module').then((m) => m.SkillsModule),
-    data: {index: 1}
+    data: { index: 1 },
   },
   {
     path: 'projects',
     loadChildren: () => import('./pages/projects/projects.module').then((m) => m.ProjectsModule),
-    data: {index: 2}
+    data: { index: 2 },
   },
-  {
-    path: 'contact',
-    loadChildren: () => import('./pages/contact/contact.module').then((m) => m.ContactModule),
-    data: {index: 4}
-  }
+  // {
+  //   path: 'contact',
+  //   loadChildren: () => import('./pages/contact/contact.module').then((m) => m.ContactModule),
+  //   data: {index: 4}
+  // }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {useHash: true})],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule],
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
